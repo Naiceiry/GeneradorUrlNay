9b6646631c1576eb241a0e88e4d8bbb0aef9148f
Naiceiry <naiceirymp@gmail.com>
2021-10-20 18:48:58 +0000

    hecho..

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-let one = ["the/", "our/"];
-let two = ["great/", "big/"];
-let three = ["jogger/", "racoon/"];
-let ext = [".com/", ".tal/"];
+let one = ["the", "our"];
+let two = ["great", "big"];
+let three = ["jogger", "help"];
+let ext = [".es", ".tal"];
 console.log(ext);
 window.onload = function() {
   document.querySelector("#resultado").innerHTML = `<ul>${generadorarray(
@@ -25,6 +25,13 @@ function generadorarray(one, two, three, ext) {
       for (let k = 0; k < three.length; k++) {
         for (let h = 0; h < ext.length; h++) {
           resultado.push(`<li>${one[i]}${two[j]}${three[k]}${ext[h]}</li>`);
+          if (ext == ".es") {
+            resultado.push(
+              `<li color=blue>${one[i]}${two[j]}genial${ext[h]}</li>`
+            );
+          } else {
+            resultado.push(`<li>${one[i]}${two[j]}elemen${ext[h]}</li>`);
+          }
         }
       }
     }
