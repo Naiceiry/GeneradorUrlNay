563e80358bba7b1c0fd50a5afa9932a8ce157271
Naiceiry <naiceirymp@gmail.com>
2021-10-20 18:29:12 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-let one = ["the", "our"];
-let two = ["great", "big"];
-let three = ["jogger", "racoon"];
-let ext = [".com", ".es", ".ue"];
+let one = ["the/", "our/"];
+let two = ["great/", "big/"];
+let three = ["jogger/", "racoon/"];
+let ext = [".com/", ".tal/"];
+console.log(ext);
 window.onload = function() {
   document.querySelector("#resultado").innerHTML = `<ul>${generadorarray(
     one,
@@ -17,13 +18,13 @@ window.onload = function() {
   )}</ul>`;
   //console.log("Hello Rigo from the console!");
 };
-function generadorarray(uno, dos, tres) {
+function generadorarray(one, two, three, ext) {
   let resultado = [];
-  for (let i = 0; i < uno.length; i++) {
-    for (let j = 0; j < dos.length; j++) {
-      for (let k = 0; k < tres.length; k++) {
+  for (let i = 0; i < one.length; i++) {
+    for (let j = 0; j < two.length; j++) {
+      for (let k = 0; k < three.length; k++) {
         for (let h = 0; h < ext.length; h++) {
-          resultado.push(`<li>${uno[i]}${dos[i]}${tres[i]}.com</li>`);
+          resultado.push(`<li>${one[i]}${two[j]}${three[k]}${ext[h]}</li>`);
         }
       }
     }
